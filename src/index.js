fc37fb1b1e95bf90839fad801b8749576380db86
r0o0 <rayeoh7@gmail.com>
2018-07-05 23:58:37 +0900

    add hoodie store event on button click

--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
+import Hoodie from '../.hoodie/client';
 import './assets/CSS/reset.sass';
 import './assets/CSS/main.sass';
+
+// Hoodie supports the following:
+console.log('hoodie', Hoodie);
+
+const form = document.querySelector('form');
+const button = document.querySelector('button');
+const input_title = title;
+const input_desc = description;
+
+button.addEventListener('click', () => { test() });
+
+let test = () => {
+  const val_title = input_title.value;
+  const val_desc = input_desc.value;
+  
+  Hoodie.store.withIdPrefix("room-").add({
+    title: val_title,
+    description: val_desc
+  }).then(note =>
+    console.log(note)).catch(err =>
+      console.error(err));
+}
+
+const list = Hoodie.store.withIdPrefix("room-").findAll().then(items => {
+  for (let item of items) {
+    console.log(item);
+  }
+});
