df4bdac2d0cf974f6176510ba8ab03c12b8f92df
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-07 22:02:50 +0900

    remove all js related to form - [converting to SPA]

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import Hoodie from '../.hoodie/client';
 import './assets/CSS/reset.sass';
 import './assets/CSS/main.sass';
+// import createroom from './assets/JS/Views/createroom';
+import chatroom from './assets/JS/Pages/chatroom';
 
 // Hoodie supports the following:
 console.log('hoodie', Hoodie);
 
-const form = document.querySelector('form');
-const button = document.querySelector('button');
-const input_title = title;
-const input_desc = description;
 
-button.addEventListener('click', () => { test() });
-
-let test = () => {
-  const val_title = input_title.value;
-  const val_desc = input_desc.value;
-  
-  Hoodie.store.withIdPrefix("room-").add({
-    title: val_title,
-    description: val_desc
-  }).then(note =>
-    console.log(note)).catch(err =>
-      console.error(err));
-}
-
-const list = Hoodie.store.withIdPrefix("room-").findAll().then(items => {
-  for (let item of items) {
-    console.log(item);
-  }
-});
+console.log('check hash', window.location.hash);
