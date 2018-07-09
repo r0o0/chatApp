233457f9c9da0dd8d887e4f99ffe514bd75a7741
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-09 18:28:28 +0900

    add nav component in create page

--- src/assets/JS/Pages/createroom.js
+++ src/assets/JS/Pages/createroom.js
@@ -1,18 +1,10 @@
 import header from '../Components/header';
 import Render from '../Render';
-// import { home, router } from './chatroom';
-import Router from '../Router';
+import nav from '../Components/gnb';
 
 const create = new Render('.app', 'create');
 
 create.addComponent({name: 'header', html: header()});
-
-// create.addComponent({name: 'nav', html: nav()});
-
-// create.addComponent({name: 'chatlist', html: chatlist()})
-
-// const createRouter = new Router(create);
-// createRouter.addRoute('create', '^#/create$');
-// console.log('route', router.routes[0].url);
+create.addComponent({name: 'nav', html: nav()});
 
 export default create;
