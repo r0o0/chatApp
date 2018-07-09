72e01fc21bee6181c54d8176d09d22de12a79cbd
r0o0 <rayeoh7@gmail.com>
2018-07-09 23:00:39 +0900

    add comment and refactor index.js

--- src/index.js
+++ src/index.js
@@ -1,57 +1,29 @@
+// hoodie
 import Hoodie from '../.hoodie/client';
-import './assets/CSS/reset.sass';
-import './assets/CSS/main.sass';
-import Render from './assets/JS/Render';
+
+// Router
 import Router from './assets/JS/Router';
+
+// Pages
 import create from './assets/JS/Pages/createroom';
 import home from './assets/JS/Pages/chatroom';
 
+// CSS
+import './assets/CSS/reset.sass';
+import './assets/CSS/main.sass';
 
 // Hoodie supports the following:
-// console.log('hoodie', Hoodie);
+console.log('hoodie', Hoodie);
 
+// 애플리케이션 라우팅 (router)
+((g = window, d = document) => {
+  const app = d.querySelector('.app');
+  const router = new Router(app);
 
-// window.addEventListener('hashchange', () => {
-//   const hash = window.location.hash;
-//   console.log('window.hash', hash);
-//   alert(hash);
-// });
-const app = document.querySelector('.app');
-// const hash = window.location.hash;
-const router = new Router(app);
-
-// console.log(pages);
-
-router.addRoute('home', '^#/home$');
-router.addRoute('create', '^#/create$');
-router.pages = { home, create };
-
-// console.log('in home', router.routes);
-// for (const route in router.routes) {
-//   const view = router.routes[route].name;
-//   if (`#${view}` === hash) {
-//     router.route
-//   }
-// }
-// console.log(router.pages = {home, create});
-// router.view = 'home';
-// if (hash === router)
-// router.components = home.components;
-// for (let route in router.routes) {
-//   const view = router.routes[route].name;
-//   if (`#${view}` === hash) {
-//     for (const page in pages) {
-//       const each = pages[page];
-//       if (view === each.view) {
-//         router.components = each.components;
-//         console.log(view, router.components);
-//       }
-//     }
-//   }
-// }
-
-
-// const main = document.querySelector('#home');
-// main.addEventListener('click', () => {
-//   console.log('click');
-// });
+  // 앱의 모든 페이지들을 루트로 추가
+  router.addRoute('home', '^#/home$');
+  router.addRoute('create', '^#/create$');
+  
+  // 라우터의 pages 속성에 render할 페이지들 참조
+  router.pages = { home, create };
+})();
