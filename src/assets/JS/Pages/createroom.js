7fcb6e65c5497c065b1908678a5e903cd01cd6fe
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-11 22:38:42 +0900

    fix render object param

--- src/assets/JS/Pages/createroom.js
+++ src/assets/JS/Pages/createroom.js
@@ -2,9 +2,9 @@ import header from '../Components/header';
 import Render from '../Render';
 import nav from '../Components/gnb';
 
-const create = new Render('.app', 'create');
+const create = new Render('create');
 
-create.addComponent({name: 'header', html: header()});
-create.addComponent({name: 'nav', html: nav()});
+create.addComponent({ name: 'header', html: header() });
+create.addComponent({ name: 'nav', html: nav() });
 
 export default create;
