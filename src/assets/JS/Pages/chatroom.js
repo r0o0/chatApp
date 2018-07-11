b8444e338fa1cd06565868ae149d3bc461145964
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-11 22:39:41 +0900

    fix render object param & remove unnecessary codes

--- src/assets/JS/Pages/chatroom.js
+++ src/assets/JS/Pages/chatroom.js
@@ -2,27 +2,16 @@ import header from '../Components/header';
 import nav from '../Components/gnb';
 import chatlist from '../Components/chatlist';
 import Render from '../Render';
-import Router from '../Router';
 
-const home = new Render('.app', 'home');
+const home = new Render('home');
 
-home.addComponent({
-  name: 'header',
-  html: header()
-});
+home.addComponent({name: 'header', html: header()});
 
-home.addComponent({
-  name: 'nav',
-  html: nav()
-});
+home.addComponent({name: 'nav', html: nav()});
 
-home.addComponent({
-  name: 'chatlist',
-  html: chatlist()
-})
+home.addComponent({name: 'chatlist', html: chatlist()})
 
-// const router = new Router(home);Router.addRoute
-// router.addRoute('home', '^#/home$');
-// console.log('route', router.routes[0].url);
+// const router = new Router(home);Router.addRoute router.addRoute('home',
+// '^#/home$'); console.log('route', router.routes[0].url);
 
 export default home;
