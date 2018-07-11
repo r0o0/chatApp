d7cbd8e459cc42401e61e42db2e379cf60b66476
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-11 22:36:56 +0900

    refactor index

--- src/index.js
+++ src/index.js
@@ -16,14 +16,14 @@ import './assets/CSS/main.sass';
 console.log('hoodie', Hoodie);
 
 // 애플리케이션 라우팅 (router)
-((g = window, d = document) => {
-  const app = d.querySelector('.app');
-  const router = new Router(app);
+(() => {
+  // router 객체 생성 및 컨테츠가 들어갈 영역을 .app으로 설정
+  const router = new Router('.app');
 
   // 앱의 모든 페이지들을 루트로 추가
   router.addRoute('home', '^#/home$');
   router.addRoute('create', '^#/create$');
-  
+
   // 라우터의 pages 속성에 render할 페이지들 참조
   router.pages = { home, create };
 })();
