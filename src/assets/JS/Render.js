f5bc1cd34151b8ac34d32ddbdfe1313538c189b5
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-12 14:50:08 +0900

    add comments to Render

--- src/assets/JS/Render.js
+++ src/assets/JS/Render.js
@@ -1,36 +1,39 @@
 // Rendering object
 'use strict';
 
+// Render 객체 생성 
+// (base class of Router)
 class Render {
+  // render 객체의 속성들
   constructor(page) {
     this.view = page;
     this.components = {};
   }
+  // 컴포넌트 추가 해주는 메서드
   addComponent(component) {
     this.components[component.name] = component;
   }
+  // 컴포넌트들을 페이지에 보여주는 메서드
   showComponent(page) {
     const hash = window.location.hash;
     const content = [];
-    console.log('show', typeof page);
+    // console.log('show', typeof page);
     if (`#${page.view}` === hash) {
       for (let component in page.components) {
         content.push(page.components[component].html);
       }
       this.updateView(content.join(''));
     } else {
-      console.log(page, '404');
+      // console.log(page, '404');
       this.updateView('<h3>404 Page Not Found</h3>');
     }
   }
+  // 보여줄 컴포넌트를 준비해주는 메서드
   updateView(content) {
-    console.log('update', content);
     content ? 
       this.app.innerHTML = content : 
       console.error('need content to update view');
   }
 }
-
-console.log('Render class', typeof new Render);
 
 export default Render;
