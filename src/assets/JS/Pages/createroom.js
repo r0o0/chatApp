a00cad2d0ee88e41533d5db1f8333e1e615bc40c
r0o0 <32406091+r0o0@users.noreply.github.com>
2018-07-16 18:26:31 +0900

    refactor createroom store function

--- src/assets/JS/Pages/createroom.js
+++ src/assets/JS/Pages/createroom.js
@@ -13,42 +13,25 @@ create.add({ name: 'form', html: formCreate() });
 
 const store = () => {
   console.log('it works');
-  console.log(Hoodie);
   const app = document.querySelector('.app');
   const form = document.querySelector('form');
   const button = document.querySelector('button');
-  console.log(form);
-  console.log(app.childNodes);
-  console.log(button);
-  const input_title = 'title';
-  const input_desc = 'description';
+  const input_title = title;
+  const input_desc = description;
 
   button.addEventListener('click', () => {
-    test()
+    storeData();
   });
 
-  let test = () => {
+  const storeData = () => {
     const val_title = input_title.value;
     const val_desc = input_desc.value;
 
-    Hoodie
-      .store
-      .withIdPrefix("room-")
-      .add({title: val_title, description: val_desc})
+    Hoodie.store.withIdPrefix("room-")
+      .add({ title: val_title, description: val_desc })
       .then(note => console.log(note))
       .catch(err => console.error(err));
   }
-
-  const list = Hoodie
-    .store
-    .withIdPrefix("room-")
-    .findAll()
-    .then(items => {
-      for (let item of items) {
-        console.log(item);
-      }
-    });
-  console.log('findall', list);
 }
 
 export { create as create };
